31239fe7151d2a0735076bea512f0e3d22319693
Angga Gantira <angga.gantira@gmail.com>
2021-05-25 17:32:27 +0700

    Ubah foto profile

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -3,10 +3,10 @@
         <ul class="nav metismenu" id="side-menu">
             <li class="nav-header">
                 <div class="dropdown profile-element">
-                    <img alt="image" class="rounded-circle" src="{{ asset('vendor/img/profile_small.jpg') }}"/>
+                    <img alt="image" class="rounded-circle" src="https://ui-avatars.com/api/?background=random&name={{ auth()->user()->name}}" />
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                         <span class="block m-t-xs font-bold">{{ auth()->user()->name }}</span>
-                        <span class="text-muted text-xs block">Art Director <b class="caret"></b></span>
+                        <span class="text-muted text-xs block">Technical Solver <b class="caret"></b></span>
                     </a>
                     <ul class="dropdown-menu animated fadeInRight m-t-xs">
                         <li><a class="dropdown-item" href="profile.html">Profile</a></li>
